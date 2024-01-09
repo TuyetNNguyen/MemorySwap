2f3f77bf48f487f2632fcd81c7b5524772f3b832
Tuyet Nguyen <tuyetnguyenww@gmail.com>
2024-01-08 16:08:01 -0800

    Created Log in feature

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,25 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username','password')
+
+    widgets = {
+        'username': forms.TextInput(
+            attrs={'placeholder': 'Your username', 'class': 'w-full py-4 px-6 rounded-xl purple-input'}),
+        'password': forms.PasswordInput(
+            attrs={'placeholder': 'Your password', 'class': 'w-full py-4 px-6 rounded-xl purple-input'})
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        for field, widget in self.widgets.items():
+            self.fields[field].widget.attrs.update(widget.attrs)
+
 
 class SignUpForm(UserCreationForm):
     class Meta:
